7745278ddd06b030ea31be37dfbe4d3aff5f9083
tmpmachine <bill.yosafatz@gmail.com>
2024-06-27 04:18:06 +0000

    added simple PWA source code

---
+++ youtube/simple-manual-cache-pwa/README.md
@@ -0,0 +1,24 @@
+# Simple Manual Cache PWA
+
+This is minimal setup to build a PWA where the user manually cache the application data to enable offline access.
+
+`manifest-cache.json` contains a list of files you want to cache. The key is mean for grouping files to keep things neat, so the name is up to you.
+```js
+{
+    "js": [
+        // ...
+    ],
+    "libraries": [
+        // ...
+    ],
+    "etc": [
+        // ...
+    ],
+    "root": [
+        "./",
+        "index.html",
+        "manifest.json",
+        "pwa-cacher.js"
+    ]
+}
+```
---
+++ youtube/simple-manual-cache-pwa/pwa-cacher.js
@@ -0,0 +1,65 @@
+let pwaCacher = (function() {
+  
+    let cacheName = 'myapp-NDQ0OTkzMjY';
+    let cacheIndexFile = '/manifest-cache.json';
+    
+    let SELF = {
+      ClearCache,
+      Cache,
+    };
+    
+    function extractUrlsFromJson(json) {
+      let urls = [];
+      for (let key in json) {
+        if (Array.isArray(json[key])) {
+          urls = urls.concat(json[key]);
+        }
+      }
+      return urls;
+    }
+      
+    function ClearCache() {
+      let isConfirm = window.confirm('Are you sure?');
+      if (!isConfirm) return;
+      
+      caches.delete(cacheName)
+        .then(() => {
+            alert('Done! Reload to take effect.');
+        });
+    };
+    
+    function Cache() {
+      
+      fetch(cacheIndexFile)
+      .then(res => res.json())
+      .then(json => {
+        
+        let cacheURLs = extractUrlsFromJson(json);
+    
+        caches.delete(cacheName)
+        .then(() => {
+          caches.open(cacheName)
+          .then(function(cache) {
+            return Promise.all(
+              cacheURLs.map(function(url) {
+                return cache.add(url).catch(function(error) {
+                  console.error('Failed to cache URL:', url, error);
+                });
+              })
+            );
+          })
+          .then(function() {
+            alert('Done! Reload to take effect.');
+          })
+          .catch(function(error) {
+            alert('Failed. Check console.');
+          });
+        });
+      
+      });
+      
+    };
+    
+    return SELF; 
+    
+})();
